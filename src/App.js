537e8449d7afb86035489ae1a5b4ecf5e707a3b3
Chen-Huayi <1035766902@qq.com>
2022-07-11 18:48:54 -0500

    prepare

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Login from "./pages/Login";
+import Layout from "./pages/Layout";
+import { Button } from 'antd';
 
 function App() {
   return (
-    <div className="App">
-      app
-    </div>
-  );
+
+      <BrowserRouter>
+          <div className="App">
+              <Button type="primary">Primary Button</Button>
+              <Routes>
+                  <Route path='/' element={<Layout></Layout>}></Route>
+                  <Route path='/login' element={<Login></Login>}></Route>
+              </Routes>
+          </div>
+      </BrowserRouter>
+
+  )
 }
 
 export default App;
---
+++ src/pages/Login/index.js
@@ -0,0 +1,15 @@
+import {Card} from "antd";
+// import logo from 'src/assets/xPQd8DhT.png'
+
+function Login() {
+    return (
+        <div className="login">
+            <Card className="login-container">
+                {/*<img className="login-logo" src={logo} alt="" />*/}
+                {/* 登录表单 */}
+            </Card>
+        </div>
+    )
+}
+
+export default Login

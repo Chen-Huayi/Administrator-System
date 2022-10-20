ff5c71acf6dac15228d8c8dd32814415090d5909
HUAYI <48390096+ChenHuayi131@users.noreply.github.com>
2022-10-19 22:22:19 -0500

    Fix package.json file indentation

--- backend/db/server.js
+++ backend/db/server.js
@@ -2,20 +2,20 @@ const mysql = require('mysql')
 // const fs = require("fs");
 
 // Create a database connection object
-// const db = mysql.createConnection({
-//     host: "my-azure-server.mysql.database.azure.com",
-//     user: "chenhuayi",
-//     password: "0131)!#!Wyy",
-//     database: "react-db",
-//     port: 3306,
-//     // ssl: {ca: fs.readFileSync("{ca-cert filename}")}
-// });
+const db = mysql.createConnection({
+    host: "my-azure-server.mysql.database.azure.com",
+    user: "chenhuayi",
+    password: "0131)!#!Wyy",
+    database: "react-db",
+    port: 3306,
+    // ssl: {ca: fs.readFileSync("{ca-cert filename}")}
+});
 
-const db = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '0131',
-    database: 'react-db',
-})
+// const db = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '0131',
+//     database: 'react-db',
+// })
 
 module.exports = db

55825072fb55140d674af3c3a11ddb82f8907132
Chen-Huayi <1035766902@qq.com>
2022-10-31 01:55:17 -0500

    Add db through mongoTest.js connections

--- backend/db/mongoTest.js
+++ backend/db/mongoTest.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 mongoose.connect('mongodb://localhost:27017/mongo_use')
-
 mongoose.connection.once('open', ()=>{
     console.log('连接成功')
 })
@@ -17,23 +16,41 @@ const stuSchema=new Schema({
     address: String
 })
 
-// Model
-const StuModel=mongoose.model('student', stuSchema)
-StuModel.create({
-    name: '林子焮',
-    age: 23,
-    gender: 'female',
-    address: '温泉公园'
-}, (err)=>{
-    if (!err){
-        console.log('插入成功')
-    }
+const StuModel=mongoose.model('students', stuSchema)  // Model (collections集合)
+
+// StuModel.create(
+//     {name: 'llw', age: 22, gender: 'female', address: '金海园'},
+//     (err)=>{ if (!err) console.log('插入成功')}
+// )
+// StuModel.find({}, {name: 1, _id: 0}, (error, result)=>{
+//     if (!error)
+//         console.log(result)
+// })
+// StuModel.updateOne({name: 'szw'}, {$set: {age: 23}}, (err)=>{
+//     if (!err)
+//         console.log('修改成功')
+// })
+// StuModel.find({}, 'name age -_id', {skip: 1, limit: 1}, (error, result)=>{
+//     if (!error)
+//         console.log(result)
+//     console.log(result instanceof StuModel)
+// })
+StuModel.count({}, (error, result)=>{
+    if (!error)
+        console.log(result)
 })
 
 
-mongoose.connection.once('close', ()=>{
-    console.log('连接已断开')
-})
 
-mongoose.disconnect()
+
+
+
+
+
+
+
+
+
+mongoose.connection.once('close', ()=>{console.log('连接已断开')})
+// mongoose.disconnect()
 
